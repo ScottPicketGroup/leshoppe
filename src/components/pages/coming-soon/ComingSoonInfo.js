097753740399d0629ca5eb1c5d9b6b355591138c
marek <marekreid@gmail.com>
2021-05-04 12:31:27 +0200

    fixed mobile margin bottom on coming soon text

--- src/components/pages/coming-soon/ComingSoonInfo.js
+++ src/components/pages/coming-soon/ComingSoonInfo.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { P } from '../../styled-components/GlobalStyles'
 import Subscribe from '../reusable/email-subscription/subscribe'
+import styled from 'styled-components'
 const ComingSoonInfo = () => {
     return (
         <>
             <h1>Coming Soon</h1>
-            <P bc2
-            style={{marginBottom: `3.34rem`}}
-            >Sign up to our newletter for news, stories and invitations to the things we love to share and enjoy from Scott Pickett Group.</P>
+            <PL bc2
+            
+            >Sign up to our newletter for news, stories and invitations to the things we love to share and enjoy from Scott Pickett Group.</PL>
             <Subscribe/>
         </>
     )
 }
 export default ComingSoonInfo
+
+export const PL = styled(P) `
+margin-bottom: 3.36rem;
+@media  screen and (max-width: 450px){
+    margin-bottom: 2rem;
+}
+`
