a1b230be71d469667e48ce9f786e086ee6667c5f
marek <marekreid@gmail.com>
2021-05-17 13:54:03 +0200

    fixed shop issues and transfered to new shop

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({ siteTitle, menuOpen, setMenuOpen }) => {
       <ComingSoonContainer
       
       >
-        {process.env.SHOPIFY_ACCESS_TOKEN}
+        
 
         {!menuOpen ? (
           <P bc1
--- src/components/pages/shop/Shop.js
+++ src/components/pages/shop/Shop.js
@@ -1,21 +1,34 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import { graphql, useStaticQuery } from "gatsby"
 import ShopCatagoryDropdown from "./shop-components/dropdown/ShopCatagoryDropdown"
 import ShopIntro from "./shop-components/ShopIntro"
 import ProductList from "./shop-components/product-list/ProductList"
 const Shop = () => {
+  
+ 
+    const data = useStaticQuery(graphql`
+    query Catagories {
+      allShopifyProduct {
+        edges {
+          node {
+            productType
+          }
+        }
+      }
+    }
+  `)
+ useEffect(() => {
+  data.allShopifyProduct.edges.map(catagory => setCatagories(catagories => [...catagories, catagory.node.productType]))
+ }, [])
   const [catagories, setCatagories] = useState([
     "All Products",
-    "Homeware",
-    "Bread",
-    "Cutlery",
-    "Meat"
+    
   ])
   const [catagory, setCatagory] = useState("Sort By Catagory")
 
   return (
-    <div>
+    <>
       <ShopIntro />
       <ShopCatagoryDropdown
         catagory={catagory}
@@ -24,7 +37,7 @@ const Shop = () => {
         setCatagories={setCatagories}
       />
       <ProductList catagory={catagory} />
-    </div>
+    </>
   )
 }
 
