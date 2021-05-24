22d9336198a26d4ffcdcf0ca02eae4380c422d23
marek <marekreid@gmail.com>
2021-05-24 04:28:53 +0200

    form send

--- src/components/pages/contact/contact-us/ContactUsForm.js
+++ src/components/pages/contact/contact-us/ContactUsForm.js
@@ -50,35 +50,16 @@ const ContactUsForm = () => {
     : setError(error => ({ ...error, message: false}) )
   }
 
-  const [serverState, setServerState] = useState({
-    submitting: false,
-    status: null
-  });
-  const handleServerResponse = (ok, msg, form) => {
-    setServerState({
-      submitting: false,
-      status: { ok, msg }
-    });
-    if (ok) {
-      form.reset();
-    }
-  };
-
+  
   const handleSubmit = e => {
     e.preventDefault()
     checkForm()
-    setServerState({ submitting: true });
-      axios({
-        method: "post",
-        url: "https://getform.io/f/4113f349-e99f-495a-b529-18c2cb40e11c",
-        data: new FormData(form)
-      })
-        .then(r => {
-          handleServerResponse(true, "Thanks!", form);
-        })
-        .catch(r => {
-          handleServerResponse(false, r.response.data.error, form);
-        });
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(inputs).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+      alert(error))
     
     
   }
@@ -87,7 +68,7 @@ const ContactUsForm = () => {
         {!thankyou ? (
             <>
  <SignUp 
-method="post" action="https://marekreid.getform.com/egokk">
+name="contact" method="POST" data-netlify="true">
  <ContactDetailsContainer>
      <ContactFormRow>
      <InputContainer>
