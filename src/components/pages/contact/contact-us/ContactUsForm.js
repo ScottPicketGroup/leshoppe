ffeab91cd421aee541b21a3bbefe2f5b093b4afc
marek <marekreid@gmail.com>
2021-05-24 03:35:09 +0200

    form send

--- src/components/pages/contact/contact-us/ContactUsForm.js
+++ src/components/pages/contact/contact-us/ContactUsForm.js
@@ -52,26 +52,19 @@ const ContactUsForm = () => {
   const handleSubmit = e => {
     e.preventDefault()
     checkForm()
-    if ( inputs.fName && inputs.sName && inputs.email.includes("@") && inputs.number && inputs.message)    
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": e.target.getAttribute("name"),
-        ...inputs
-      })
-    }).then(setThankyou(true)  )
+    if ( inputs.fName && inputs.sName && inputs.email.includes("@") && inputs.number && inputs.message)  setThankyou(true)    
+    
   }
   return (
     <ContactUsFormContainer>
         {!thankyou ? (
             <>
  <SignUp 
- 
+ netlify
  data-netlify="true" 
  data-netlify-honepot="bot-field"
- action="POST"
- name="contact-form">
+ method="post"
+ name="contact">
  <ContactDetailsContainer>
      <ContactFormRow>
      <InputContainer>
