25ecb1f2e75fc9e3b4f8bd381f804caf0384c1dd
marek <marekreid@gmail.com>
2021-06-11 16:23:13 +0200

    changed to mp4 locally hosted

--- src/components/pages/landing/sections/video/videoPlayer.jsx
+++ src/components/pages/landing/sections/video/videoPlayer.jsx
@@ -3,54 +3,49 @@ import React, {useState, useRef, useEffect} from 'react'
 import ReactPlayer from 'react-player'
 import styled from 'styled-components'
 import {gsap, TweenLite} from 'gsap'
+import {P} from '../../../../styled-components/GlobalStyles'
+import logo from '../../../../../images/logos/leshoppe.svg'
 import video from '../../../../../images/introVideo.mp4'
 const VideoPlayer = ({url}) => {
   const [sound, setSound] = React.useState(true)
   const [opacity, setOpacity] = React.useState(`100%`)
   const [playing, setPlaying] = React.useState(false)
   let player = useRef(null)
+  let overlay = useRef(null)
 
   
   useEffect(() => {
-    TweenLite.fromTo(player, .5, {
+    TweenLite.fromTo(overlay, .5, {
       autoAlpha: 1,
       
       delay: 1
     }, {
       autoAlpha: 0,
-      delay: 1
+      delay: 2
      
     })
     setPlaying(true)
   },[])
 
+  const controlMute = () => {
+    player.muted = !player.muted
+  }
+
   console.log(player)
       return (
-        <ImgLandscape >
-        <Background ref={el => (player = el)}/>
-          <Video autoPlay muted loop>
+        <ImgLandscape data-sal="fade"
+        data-sal-delay="100"
+        data-sal-duration="500"
+        data-sal-easing="ease">
+        <Background ref={el => (overlay = el)}>
+          <img src={logo} alt="le shoppe logo loading" />
+          <P bc3 style={{color:`white`}}>Loading ...</P>
+        </Background>
+          <Video autoPlay muted loop ref={el => (player = el)}>
           <source src={video} type="video/mp4" />
           </Video>
         
-          { !sound ? (
-                <Mute
-                onClick={() => setSound(true)}
-                >
-                  Mute
-                  </Mute>
-          )
-            :
-            (
-                <Mute
-                onClick={() => setSound(false)}
-                >
-               
-               
-                
-                  Unmute
-                  </Mute>
-            )
-        }
+          <button onClick={controlMute} >unmute</button>
      
         </ImgLandscape>
       )
@@ -78,6 +73,19 @@ z-index: 2;
 height: 100%;
 width: 100%;
 background: black;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+transition: all 1s ease-ine;
+img {
+  opacity: 1;
+  stroke: white;
+  fill: white;
+  z-index: 3;
+  height: 35%;
+  width: 35%;
+}
 `
 
   export const ImgLandscape = styled.div`
