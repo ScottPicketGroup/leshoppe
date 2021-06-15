04701517d46f1e96a7ea8d0941d3bc017c17cf6c
Marek J. Reid <marekreid@gmail.com>
2021-06-15 06:16:39 +0200

    Update shop.js

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -5,7 +5,7 @@ import ShopLanding from "../components/pages/shop/ShopLanding"
 const Shop = () => (
     <Layout>
       <SEO title="Shop" />
-      <ShopLanding />
+      // shop disabled until required <ShopLanding />
     </Layout>
 )
 
