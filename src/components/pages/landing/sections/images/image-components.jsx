350a04a419ef5433a7cd9296eb834b6401abbe13
Marek J. Reid <marekreid@gmail.com>
2021-07-07 13:05:11 +0200

    Update image-components.jsx

--- src/components/pages/landing/sections/images/image-components.jsx
+++ src/components/pages/landing/sections/images/image-components.jsx
@@ -14,6 +14,6 @@ export const ImgLandscape = styled(Img)`
   @media screen and (max-width: 450px) {
     width: 75%;
     align-self: flex-end;
-    margin-bottom: ${ props => props.marginBottom ? props.marginBottom : ``};
+    margin-bottom: ${(props) => (props.marginBottom ? props.marginBottom : 0)};
   }
 `
