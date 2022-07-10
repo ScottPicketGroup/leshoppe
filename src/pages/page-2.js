b8b1ad79a4cb1ac2cb5c4fed89ba672267aacefd
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-07-10 12:39:25 +0200

    remvoed 404

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,16 @@
-import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import GlobalStateProvider from "../components/context/GlobalStateProvider"
+// import React from "react"
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
+// import GlobalStateProvider from "../components/context/GlobalStateProvider"
 
-const SecondPage = (menuOpen, setMenuOpen) => {
-  return (
-    <GlobalStateProvider>
-      <Layout>
-        <SEO title="Home" />
-      </Layout>
-    </GlobalStateProvider>
-  )
-}
+// const SecondPage = (menuOpen, setMenuOpen) => {
+//   return (
+//     <GlobalStateProvider>
+//       <Layout>
+//         <SEO title="Home" />
+//       </Layout>
+//     </GlobalStateProvider>
+//   )
+// }
 
-export default SecondPage
+// export default SecondPage
