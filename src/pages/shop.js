3b30a4ac957388954a7d1a6a6e38628b71d91424
Marek Reid <mjr@Mareks-MacBook-Pro.local>
2022-07-11 08:49:27 +0200

    removed shop

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,13 +1,13 @@
-// import * as React from "react"
-// import Layout from "../components/layout"
-// import SEO from "../components/seo"
-// import ShopLanding from "../components/pages/shop/ShopLanding"
-// const Shop = () => (
-//     <Layout>
-//       <SEO title="Shop" />
-//       {/* <ShopLanding /> */}
-//     </Layout>
-// )
+import * as React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import ShopLanding from "../components/pages/shop/ShopLanding"
+const Shop = () => (
+    <>
+      <SEO title="Shop" />
+      {/* <ShopLanding /> */}
+    </>
+)
 
-// export default Shop
+export default Shop
 
